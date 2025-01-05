f8f8a873f0ad6b51a3372bb0722c9d10f87f8938
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2025-01-05 11:49:32 +0530

    Added notification model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyparser = require("body-parser");
 const app = express();
 const { PORT } = require("./config/serverConfig");
 const { sendBasicEmail } = require("./service/email-service");
+var cron = require("node-cron");
 
 const setupAndStartServer = () => {
   app.use(bodyparser.json());
@@ -12,12 +13,16 @@ const setupAndStartServer = () => {
   app.listen(PORT, async () => {
     console.log(`server started at port ${PORT}`);
 
-    sendBasicEmail(
-      "support@gmail.com",
-      "xyz@gmail.com",
-      "This is a test email",
-      "Hey, how are you, I hope you like the support"
-    );
+    // sendBasicEmail(
+    //   "support@gmail.com",
+    //   "xyz@gmail.com",
+    //   "This is a test email",
+    //   "Hey, how are you, I hope you like the support"
+    // );
+
+    cron.schedule("* * * * *", () => {
+      console.log("running a task every minute");
+    });
   });
 };
 
